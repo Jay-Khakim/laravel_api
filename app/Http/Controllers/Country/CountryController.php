a5d3a39aef81f3f918cb1a629b2dbf6439bae7fc
Jay-Khakim <mgmediajay@gmail.com>
2020-10-14 17:34:36 +0500

    country controller, model, api.php

---
+++ app/Http/Controllers/Country/CountryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Country;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\CountryModel;
+class CountryController extends Controller
+{
+    public function country(){
+        return response()->json(CountryModel::get(), 200);
+    }
+
+    public function countryById($id){
+        return response()->json(CountryModel::find($id), 200);
+    }
+
+    public function countrySave(Request $req){
+        $country = CountryModel::create($req->all());
+        return response()->json($country, 201);
+    }
+}
